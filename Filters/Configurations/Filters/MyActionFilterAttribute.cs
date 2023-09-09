df0a8ca067b123e2144e3f3598057a82d7baa679
yogekgagdev <yogekgarg17@gmail.com>
2023-09-09 14:56:52 +0530

    controller and route level fitlers using attribute class inheritance

---
+++ Filters/Configurations/Filters/MyActionFilterAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Filters.Configurations.Filters
+{
+    public class MyActionFilterAttribute : Attribute, IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            Console.WriteLine($"Executing MyActionFilterAttribute: OnActionExecuting");
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            Console.WriteLine($"Executing MyActionFilterAttribute: OnActionExecuted");
+        } 
+    }
+}
--- Filters/Controllers/DriversController.cs
+++ Filters/Controllers/DriversController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Filters.Configurations.Filters;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Filters.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [MyActionFilterAttribute]
     public class DriversController : ControllerBase
     {
        private List<string> MyDrivers = new List<string>()
@@ -14,18 +16,19 @@ namespace Filters.Controllers
            "Dishant Sood"
        };
 
-      [HttpGet]
-      public IActionResult Get()
-      {
-            return Ok(MyDrivers);
-      }
+    [HttpGet]
+    [MyActionFilterAttribute]
+    public IActionResult Get()
+    {
+        return Ok(MyDrivers);
+    }
 
-        [HttpPost]
-        public IActionResult Post(string DriverName)
-        {
-            MyDrivers.Add(DriverName);
-            return Ok(MyDrivers);
-        }
+    [HttpPost]
+    public IActionResult Post(string DriverName)
+    {
+        MyDrivers.Add(DriverName);
+        return Ok(MyDrivers);
+    }
 
        
     }
