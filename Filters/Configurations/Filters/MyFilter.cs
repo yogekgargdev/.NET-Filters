64c6fc5d7e745b638ca8a97b36df5fdb41ca4bfe
yogekgagdev <yogekgarg17@gmail.com>
2023-09-09 14:28:51 +0530

    Add project files.

---
+++ Filters/Configurations/Filters/MyFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Filters.Configurations.Filters
+{
+    public class MyFilter : IActionFilter
+    {
+        // Imp Note:OnActionExecuting comes first in the picture, then comes OnActionExecuted()
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            Console.WriteLine($"Executing MyFilter: OnActionExecuting");
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            Console.WriteLine($"Executing MyFilter: OnActionExecuted");
+        }
+
+    }
+}
