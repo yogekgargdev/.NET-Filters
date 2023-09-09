bed66714a40358e3ddc365cdc9ebbd806ecb69f6
yogekgagdev <yogekgarg17@gmail.com>
2023-09-09 14:49:19 +0530

    global level filters

---
+++ Filters/Controllers/DriversController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Filters.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DriversController : ControllerBase
+    {
+       private List<string> MyDrivers = new List<string>()
+       {
+           "Yogek Garg",
+           "Abhinav Chandlda",
+           "Ankit Jain",
+           "Dishant Sood"
+       };
+
+      [HttpGet]
+      public IActionResult Get()
+      {
+            return Ok(MyDrivers);
+      }
+
+        [HttpPost]
+        public IActionResult Post(string DriverName)
+        {
+            MyDrivers.Add(DriverName);
+            return Ok(MyDrivers);
+        }
+
+       
+    }
+}
